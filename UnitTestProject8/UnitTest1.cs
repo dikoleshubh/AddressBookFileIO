f5e47cb97b64133b9ac31034afc7648bb5db4fa0
dikoleshubh <dikole.shubh@gmail.com>
2021-05-19 00:04:17 +0530

    User Summary & Mulitple User Fare Calculation

--- UnitTestProject8/UnitTest1.cs
+++ UnitTestProject8/UnitTest1.cs
@@ -13,11 +13,12 @@ namespace UnitTestProject8
         {
             //Creating instance of invoice generator 
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
-            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            Ride[] rides = { new Ride(2.0, 6), new Ride(0.1, 2) };
             RideRepository rideRepository = new RideRepository();
-            string userId = "Dhiraj";
+            string userId = "Mini";
             rideRepository.AddRide(userId, rides);
             Ride[] rideData = rideRepository.GetRides(userId);
+
             //Generating Summary for rides
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, 15);
